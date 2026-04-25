73c75232e58f89839d0a4bc3a6d136cc2a5961fd
Sk Sahagir <sksahagira@gmail.com>
2026-04-25 11:33:53 +0530

    Update after releas

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='py-datamorph',
+    name='py-datamorph-sks',
     version='1.0.0',
     author='Sk Sahagir',
     description='A high-performance data conversion, cleaning, and statistics library.',
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     packages=find_packages(),
     include_package_data=True,
